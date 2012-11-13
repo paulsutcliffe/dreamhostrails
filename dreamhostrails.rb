039a38b981e8070512fe18a7a9ddf3821fac0666
Paul Sutcliffe <paul@kosmyka.com>
2012-11-13 00:11:52 -0500

    New setup for using Passenger, awesome

--- dreamhostrails.rb
+++ dreamhostrails.rb
@@ -38,7 +38,6 @@ end
 CODE
 
 file "config/environment.rb", <<-CODE
-# Load the rails application
 ENV['GEM_PATH'] = File.expand_path('~/.gems') + ':/usr/lib/ruby/gems/1.8'
 
 require File.expand_path('../application', __FILE__)
